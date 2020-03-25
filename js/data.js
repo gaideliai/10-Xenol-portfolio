095ec6a40868a10e8bee87618365901f7aedf312
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-03-25 17:04:30 +0200

    Services section

--- js/data.js
+++ js/data.js
@@ -9,6 +9,38 @@
 //portfolio
 
 //services
+const services = [
+    {
+        icon: 'language',
+        title: 'Interface Design',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'optin-monster',
+        title: 'Experience Design',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'connectdevelop',
+        title: 'Web Development',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'first-order',
+        title: 'Mobile App',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'slideshare',
+        title: 'Digital Marketing',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    },
+    {
+        icon: 'themeisle',
+        title: 'Woocommerce',
+        description: 'Continually scale resource-leveling vectors without best-of-breed schemas. Assertively initiate backward-compatible'
+    }
+];
 
 //team
 
