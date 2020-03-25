9cfe853dc5ce30cd4e4e0b4687d98f8cba0c63b1
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-03-25 14:42:24 +0200

    initial commit

---
+++ js/actions.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//header
+
+//hero
+
+//about us
+
+//portfolio
+
+//services
+
+//team
+
+//numbers
+
+//pricing
+
+//blog
+
+//testimonials
+
+//get in touch
+
+//map
+
+//footer
