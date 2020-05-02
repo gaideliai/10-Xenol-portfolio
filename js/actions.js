b2745797e4d4569a847d7f8e9b18037f798ac232
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-05-02 23:30:59 +0300

    lightbox

--- js/actions.js
+++ js/actions.js
@@ -21,7 +21,27 @@ window.addEventListener('scroll', headerBackground);
 //about us
 renderSkills (skills);
 
-// document.querySelector('#about .video').addEventListener('click', () =>);
+document.querySelector('#about .video .play-btn').addEventListener('click', () => {
+    document.querySelector('.lightbox').classList.add('show');       
+})
+
+document.querySelector('.lightbox > .background').addEventListener('click', () => {
+    document.querySelector('.lightbox').classList.remove('show');
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode == 27) {
+    document.querySelector('.lightbox').classList.remove('show');
+    }    
+})
+
+// document.onkeydown = function(evt) {
+//     evt=evt || window.event;
+//     if (evt.keyCode == 27) {
+//         document.querySelector('.lightbox').classList.remove('show');
+//     }
+// }
+
 //portfolio
 renderPortfolio (works);
 
